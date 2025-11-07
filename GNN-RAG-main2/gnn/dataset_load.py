e46e04602efd826fa42ed15160436757dd05a6d4
korruz <korruz@outlook.com>
2025-11-07 10:26:23 +0800

    修改模型的绝对地址为相对地址

--- GNN-RAG-main2/gnn/dataset_load.py
+++ GNN-RAG-main2/gnn/dataset_load.py
@@ -198,7 +198,7 @@ class BasicDataLoader(object):
             elif self.tokenize == 'roberta':
                 tokenizer_name = 'roberta-base'
             elif self.tokenize  == 'sbert':
-                tokenizer_name = '/home/bi3/zxd_env/GNN-RAG-main2/gnn/sbert'
+                tokenizer_name = './sbert'
             elif self.tokenize == 'sbert2':
                 tokenizer_name = 'sentence-transformers/all-mpnet-base-v2'
             elif self.tokenize == 't5':
@@ -208,7 +208,7 @@ class BasicDataLoader(object):
             elif self.tokenize == 't5':
                 tokenizer_name = 't5-small'
             elif self.tokenize == 'relbert':
-                tokenizer_name = '/home/bi3/zxd_env/GNN-RAG-main2/gnn/pretrained_lms/sr-simbert'
+                tokenizer_name = './pretrained_lms/sr-simbert'
 
             self.max_query_word = max_count + 2 #for cls token and sep
             #self.tokenizer = AutoTokenizer(self.max_query_word)
@@ -458,7 +458,7 @@ class BasicDataLoader(object):
             elif tokenize == 'roberta':
                 tokenizer_name = 'roberta-base'
             elif tokenize == 'sbert':
-                tokenizer_name = '/home/bi3/zxd_env/GNN-RAG-main2/gnn/sbert'
+                tokenizer_name = './sbert'
             elif tokenize == 'sbert2':
                 tokenizer_name = 'sentence-transformers/all-mpnet-base-v2'
             elif tokenize == 'simcse':
@@ -466,7 +466,7 @@ class BasicDataLoader(object):
             elif tokenize == 't5':
                 tokenizer_name = 't5-small'
             elif tokenize == 'relbert':
-                tokenizer_name = '/home/bi3/zxd_env/GNN-RAG-main2/gnn/pretrained_lms/sr-simbert'
+                tokenizer_name = './pretrained_lms/sr-simbert'
 
             tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, local_files_only=True)  # <--- 添加参数
             pad_val = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
--- GNN-RAG-main2/gnn/modules/question_encoding/bert_encoder.py
+++ GNN-RAG-main2/gnn/modules/question_encoding/bert_encoder.py
@@ -36,9 +36,9 @@ class BERTInstruction(BaseInstruction):
             self.pretrained_weights = 'roberta-base'
             word_dim = 768#self.word_dim
         elif model == 'sbert':
-            self.tokenizer = AutoTokenizer.from_pretrained('/home/bi3/zxd_env/GNN-RAG-main2/gnn/sbert', local_files_only=True)
-            self.pretrained_weights = '/home/bi3/zxd_env/GNN-RAG-main2/gnn/sbert'
-            word_dim = 768  # self.word_dim
+            self.tokenizer = AutoTokenizer.from_pretrained('./sbert', local_files_only=True)
+            self.pretrained_weights = './sbert'
+            word_dim = 384  # self.word_dim
         elif model == 'simcse':
             #print('ok')
             self.tokenizer = AutoTokenizer.from_pretrained('princeton-nlp/sup-simcse-bert-base-uncased')
